dbde50aa279dadeedfc8ff7142e3c014e7c4ef78
HyperX98 <raulxiloj98@gmail.com>
2020-04-09 15:38:55 -0600

    We use the actual graphic memory

--- graphics.asm
+++ graphics.asm
@@ -1,13 +1,17 @@
 ;-------------------------------MACROS GRAPHICS---------------------------------
 
-setGraphicMode macro
+setVideoMode macro
     mov ax, 0013h
     int 10h
+    mov ax, 0A000h
+    mov ds, ax      ; DS = A000h (memoria de graficos)
 endm
 
 setTextMode macro
     mov ax, 0003h
     int 10h
+    mov ax, @data   ;to return the ds to the dataaaaa
+    mov ds,ax
 endm
 
 pressKey macro
