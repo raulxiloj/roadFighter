7e12f55b91108be360abb31876702a3174dad501
HyperX98 <raulxiloj98@gmail.com>
2020-04-10 06:47:18 -0600

    The use of game.asm in the normal user mode

--- normalU.asm
+++ normalU.asm
@@ -15,7 +15,8 @@ LOCAL menu,initGame, logout
     initGame:
         setVideoMode
         paintBoard 0fh
-        paintBackground 08
+        paintBackground 7
+        drawCar 50072        ;156,152
         getChar
         setTextMode
         jmp menu    
@@ -24,63 +25,4 @@ LOCAL menu,initGame, logout
         cleanBuffer userPass, SIZEOF userPass, 24h
         jmp menuPrincipal
 
-endm
-
-;320*i+j
-paintBoard macro color
-LOCAL top, bottom, leftSide, rightSide
-mov dl, color
-mov di, 6410        ;(20,10) 
-top:
-    mov [di], dl
-    inc di
-    cmp di, 6710    ;(20,310) 
-    jne top
-;-------------------------------------------------
-mov di, 60810       ;(190,10) 
-bottom:
-    mov [di], dl
-    inc di
-    cmp di, 61110   ;(190, 310)
-    jne bottom
-;-------------------------------------------------
-mov di, 6410        ;(20,10) 
-leftSide:
-    mov [di], dl
-    add di, 320
-    cmp di, 60810   ;(190,10)
-    jne leftSide 
-;-------------------------------------------------
-mov di, 6709        ;(20,309)  
-rightSide:
-    mov [di], dl
-    add di, 320
-    cmp di, 61109   ;(190,309)
-    jne rightSide
-endm
-
-paintBackground macro color
-LOCAL for1, for2
-    xor ax, ax    ;count rows
-    xor bx, bx    ;next row
-    xor cx, cx    ;aux
-    mov ax, 0
-    mov dl, color
-    mov di, 6731      
-    jmp for1
-    fixRow:
-        add bx, 320
-        mov di, bx   
-    for1:
-        inc ax
-        mov bx, di
-        mov cx, di
-        add cx, 298
-        for2: 
-            mov [di], dl
-            inc di 
-            cmp di, cx
-            jne for2
-            cmp ax, 169
-            jne fixRow
 endm
