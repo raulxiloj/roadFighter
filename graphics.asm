f04c28acf76828ae1eb58eeecc7059128e4239a3
HyperX98 <raulxiloj98@gmail.com>
2020-04-19 09:16:25 -0600

    Start using the extra segment to deal with graphics and add the methods to paint board, draw the car and blocks

--- graphics.asm
+++ graphics.asm
@@ -4,17 +4,314 @@ setVideoMode macro
     mov ax, 0013h
     int 10h
     mov ax, 0A000h
-    mov ds, ax      ; DS = A000h (memoria de graficos)
+    mov es, ax      ; using the extra segment for the graphics
 endm
 
 setTextMode macro
     mov ax, 0003h
     int 10h
-    mov ax, @data   ;to return the ds to the dataaaaa
-    mov ds,ax
 endm
 
 pressKey macro
     mov ah, 10h
     int 16h
+endm
+
+;-----------------------------------------------------------------------------
+;320*i+j
+paintBoard macro color
+LOCAL top, bottom, leftSide, rightSide
+    mov dl, color
+    mov di, 6410        ;(20,10) 
+    top:
+        mov es:[di], dl
+        inc di
+        cmp di, 6710    ;(20,310) 
+        jne top
+    ;-------------------------------------------------
+    mov di, 60810       ;(190,10) 
+    bottom:
+        mov es:[di], dl
+        inc di
+        cmp di, 61110   ;(190, 310)
+        jne bottom
+    ;-------------------------------------------------
+    mov di, 6410        ;(20,10) 
+    leftSide:
+        mov es:[di], dl
+        add di, 320
+        cmp di, 60810   ;(190,10)
+        jne leftSide 
+    ;-------------------------------------------------
+    mov di, 6709        ;(20,309)  
+    rightSide:
+        mov es:[di], dl
+        add di, 320
+        cmp di, 61109   ;(190,309)
+        jne rightSide
+endm
+
+;-----------------------------------------------------------------------------
+paintBackground macro color
+LOCAL for1, for2
+    xor ax, ax    ;count rows
+    xor bx, bx    ;next row
+    xor cx, cx    ;aux
+    mov ax, 0
+    mov dl, color
+    mov di, 6731      
+    jmp for1
+    fixRow:
+        add bx, 320
+        mov di, bx   
+    for1:
+        inc ax
+        mov bx, di
+        mov cx, di
+        add cx, 298
+        for2: 
+            mov es:[di], dl
+            inc di 
+            cmp di, cx
+            jne for2
+            cmp ax, 169
+            jne fixRow
+endm
+
+;-----------------------------------------------------------------------------
+drawCar macro pos 
+LOCAL body, wheels1, wheels2
+    mov di, pos
+    mov dl, 4
+
+    mov es:[di+3], dl
+    mov es:[di+4], dl
+    mov es:[di+5], dl
+    mov es:[di+6], dl
+    mov es:[di+7], dl
+    mov es:[di+8], dl
+    mov es:[di+9], dl
+    mov es:[di+10], dl
+    mov es:[di+11], dl
+    mov es:[di+12], dl
+    mov es:[di+13], dl
+    mov es:[di+14], dl
+    mov es:[di+15], dl
+    mov es:[di+16], dl
+
+    mov cx, 24
+    body:
+        add di, 320
+        mov es:[di+3], dl
+        mov es:[di+4], dl
+        mov es:[di+5], dl
+        mov es:[di+6], dl
+        mov es:[di+7], dl
+        mov es:[di+8], dl
+        mov es:[di+9], dl
+        mov es:[di+10], dl
+        mov es:[di+11], dl
+        mov es:[di+12], dl
+        mov es:[di+13], dl
+        mov es:[di+14], dl
+        mov es:[di+15], dl
+        mov es:[di+16], dl
+        loop body
+
+    ;-------------------------------
+    mov di, pos
+    mov dl, 0
+    add di, 640
+    mov es:[di], dl
+    mov es:[di+1], dl
+    mov es:[di+2], dl
+    mov es:[di+17], dl
+    mov es:[di+18], dl
+    mov es:[di+19], dl
+    mov cx, 6
+    wheels1:
+        add di, 320
+        mov es:[di], dl
+        mov es:[di+1], dl
+        mov es:[di+2], dl
+        mov es:[di+17], dl
+        mov es:[di+18], dl
+        mov es:[di+19], dl
+        loop wheels1
+    ;-------------------------------
+    add di, 2240
+    mov es:[di], dl
+    mov es:[di+1], dl
+    mov es:[di+2], dl
+    mov es:[di+17], dl
+    mov es:[di+18], dl
+    mov es:[di+19], dl
+    xor cx, cx
+    mov cx, 7
+    wheels2:
+        add di, 320
+        mov es:[di], dl
+        mov es:[di+1], dl
+        mov es:[di+2], dl
+        mov es:[di+17], dl
+        mov es:[di+18], dl
+        mov es:[di+19], dl
+        loop wheels2
+endm
+
+eraseCar macro 
+LOCAL while, while2, littleFix
+    xor bx, bx      ;next row
+    xor cx, cx      ;counter, ch for rows and cl for cols
+    mov cx, 0
+    mov dl, 7
+    mov di, currPos
+    jmp while
+    littleFix:
+        add bx, 320
+        mov di, bx
+        mov cl, 0
+    while:
+        inc ch
+        mov bx, di
+        while2:
+            mov es:[di], dl
+            inc di 
+            inc cl
+            cmp cl, 20
+            jne while2
+            cmp ch, 25
+            jne littleFix    
+endm
+
+;-----------------------------------------------------------------------------
+;Colors
+;Good block: 0Eh, 0Ch
+;Bad block: 2 ,0Ah  
+drawBlock macro pos, color1, color2  
+LOCAL while, L1, L2, L3, secondAux, finish
+    xor cx, cx
+    mov di, pos
+    mov dl, color1
+
+    while:
+        cmp cx, 4
+        jl L1
+        cmp cx, 8
+        jl L2
+        cmp cx, 12 
+        jl L3
+        cmp cx, 16
+        jl L2
+        cmp cx, 20
+        jl L1
+        jmp finish
+
+    L1:
+        inc cx
+        mov es:[di+8], dl
+        mov es:[di+9], dl
+        mov es:[di+10], dl
+        mov es:[di+11], dl
+        add di, 320
+        jmp while
+    L2:
+        inc cx
+        mov es:[di+4], dl
+        mov es:[di+5], dl
+        mov es:[di+6], dl
+        mov es:[di+7], dl
+        mov dl, color2
+        mov es:[di+8], dl
+        mov es:[di+9], dl
+        mov es:[di+10], dl
+        mov es:[di+11], dl
+        mov dl, color1
+        mov es:[di+12], dl
+        mov es:[di+13], dl
+        mov es:[di+14], dl
+        mov es:[di+15], dl
+        add di, 320
+        jmp while
+    L3:  
+        inc cx
+        mov es:[di], dl
+        mov es:[di+1], dl
+        mov es:[di+2], dl
+        mov es:[di+3], dl
+        mov dl, color2
+        mov bx, 4
+        push cx
+        mov cx, 12
+        secondAux:
+            mov es:[di + bx],dl
+            inc bl
+            loop secondAux
+        pop cx
+        mov dl, color1
+        mov es:[di+16], dl
+        mov es:[di+17], dl
+        mov es:[di+18], dl
+        mov es:[di+19], dl
+        add di, 320
+        jmp while
+
+    finish:
+
+endm
+
+eraseBlock macro pos
+LOCAL while, while2, littleFix
+    xor bx, bx      ;next row
+    xor cx, cx      ;counter, ch for rows and cl for cols
+    mov cx, 0
+    mov dl, 7
+    mov di, pos
+    jmp while
+    littleFix:
+        add bx, 320
+        mov di, bx
+        mov cl, 0
+    while:
+        inc ch
+        mov bx, di
+        while2:
+            mov es:[di], dl
+            inc di 
+            inc cl
+            cmp cl, 20
+            jne while2
+            cmp ch, 20
+            jne littleFix 
+endm
+;-----------------------------------------------------------------------------
+drawChar macro row, col, char
+    ;set cursor 
+    mov dh, row     ;row
+    mov dl, col     ;col
+    mov bh, 0       ;page
+    mov ah, 02h
+    int 10h
+    ;print char in video
+    mov al, char
+    mov bl, 15d     ;color
+    mov bh, 0       ;page
+    mov ah, 0Eh
+    int 10h
+endm
+;-----------------------------------------------------------------------------
+drawString macro row, col, string 
+LOCAL while
+    int 3
+    xor bx, bx
+    xor cx, cx
+    mov cl, col
+    getLength string 
+    while:
+        drawChar cl, row, string[bx]
+        inc cl
+        inc bx
+        dec si
+        cmp si, 0
+        jne while
 endm
