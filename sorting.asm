61c1ead77ffce94e40d5fb3074211b0581795485
HyperX98 <raulxiloj98@gmail.com>
2020-05-04 19:45:51 -0600

    Bubble sort descendet added (just change the condition xd)

--- sorting.asm
+++ sorting.asm
@@ -56,6 +56,64 @@ LOCAL for, for2, continue, continue2, swap, finish
         
 endm
 
+bubbleSortDesc macro array
+LOCAL for, for2, continue, continue2, swap, finish
+    xor ax, ax  ;al actual -  ah siguiente
+    xor bx, bx  ;access to array  
+    xor cx, cx  ;cl = cont for num of elements | ch = size of the array 
+    mov bx, 2   
+
+    mov al, 3
+    mul nElements
+    mov ch, al
+    sub ch, 3
+
+    for:
+        cmp cl, nElements
+        jge finish
+        pusha
+        statistics
+        popa
+        for2:
+            mov al, array[bx]
+            mov ah, array[bx+3] 
+            cmp al, ah
+            jl swap
+            jmp continue2
+            
+            swap:
+            mov auxSort3, al    ;pts
+            mov al, array[bx-1] ;level
+            mov auxSort2, al
+            mov al, array[bx-2] ;row
+            mov auxSort, al
+
+            mov al, array[bx+1] ;row
+            mov array[bx-2], al
+            mov al, array[bx+2] ;level;
+            mov array[bx-1], al
+            mov al, array[bx+3] ;pts
+            mov array[bx], al
+            
+            mov al, auxSort
+            mov array[bx+1], al
+            mov al, auxSort2
+            mov array[bx+2], al
+            mov al, auxSort3
+            mov array[bx+3], al
+            
+            continue2:
+            add bl, 3
+            cmp bl, ch
+            jl for2
+            mov bx, 2
+            inc cl
+            jmp for 
+        
+    finish:
+        
+endm
+
 getMax macro
 LOCAL for, continue, swap
     xor ax, ax
